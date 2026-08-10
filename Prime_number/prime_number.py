62431a76de9d6921c808191ef9849fe0d49fb0ef
Vishal <mgvishalctr@gmail.com>
2026-08-10 23:02:42 +0530

    commit

---
+++ Prime_number/prime_number.py
@@ -0,0 +1,17 @@
+def is_prime(n):
+    if n < 2:
+        return False
+
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+
+    return True
+
+
+n = int(input("Enter a number: "))
+
+if is_prime(n):
+    print(n, "is Prime")
+else:
+    print(n, "is Not Prime")
